d089292eccd246640eae885a6320f7974332de91
YehonatanKadosh <jacojacoj3@gmail.com>
2021-04-23 17:20:27 +0300

    More Singleton Examples Double lock Check Volatile Usage

--- Singelton/Program.cs
+++ Singelton/Program.cs
@@ -20,7 +20,7 @@ namespace Singelton
         {
             public int Counter1 { get; set; }
             public static Singelton1 Instance { get; } = new Singelton1();
-            private Singelton1() { }
+            private Singelton1() { }//Lock the creation of instances
         }
 
         // Big projects with multythreading
@@ -39,7 +39,53 @@ namespace Singelton
                     return instance;
                 }
             }
-            private Singelton2() { }
+            private Singelton2() { }//Lock the creation of instances
+        }
+
+        // Big Project with double checked lock
+        class Singelton3
+        {
+            public int Counter3 { get; set; }
+            readonly static object obj = new object();
+            private static Singelton3 instance;
+            public static Singelton3 Instance
+            {
+                get
+                {
+                    if (instance == null)
+                        lock (obj)
+                            if (instance == null)
+                                instance = new Singelton3();
+                    return instance;
+                }
+            }
+            private Singelton3() { } //Lock the creation of instances
+        }
+
+        // Big Project with double checked lock and Volatile use in case creating the instance takes time
+        /// <summary>
+        /// Threads Won't be able to proceed until the instance is made completly
+        /// 1. Construct an empty resource
+        /// 2. Assign to instance -- Danger of another thread using the incomplete instance (Solved by volatile)
+        /// 3. Call the constructor
+        /// </summary>
+        class Singelton4
+        {
+            public int Counter4 { get; set; }
+            readonly static object obj = new object();
+            private static volatile Singelton4 instance;
+            public static Singelton4 Instance
+            {
+                get
+                {
+                    if (instance == null)
+                        lock (obj)
+                            if (instance == null)
+                                instance = new Singelton4();
+                    return instance;
+                }
+            }
+            private Singelton4() { } //Lock the creation of instances
         }
     }
 }

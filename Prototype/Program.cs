b5767f52cb23a112bf741cdffce98a8775472cc2
YehonatanKadosh <jacojacoj3@gmail.com>
2021-04-23 18:03:08 +0300

    Comments

--- Prototype/Program.cs
+++ Prototype/Program.cs
@@ -13,10 +13,17 @@ namespace Prototype
             //Wrong way, right implementation
             Prototype1 Prototype1_0 = new Prototype1() { Counter = 3 };
             Prototype1 prototype1_1 = (Prototype1)Prototype1_0.Clone();
+            prototype1_1.Counter = 4;
 
             //Right way, right implementation
-            Prototype2 Prototype2_0 = new Prototype2() { Counter = 3 };
-            Prototype2 prototype2_1 = (Prototype2)Prototype1_0.Clone();
+            Prototype2 Prototype2_0 = new Prototype2() { Counter = 5 };
+            Prototype2 prototype2_1 = (Prototype2)Prototype2_0.Clone();
+            prototype2_1.Counter = 6;
+
+            ///Prototype1_0.Counter = 3
+            ///Prototype1_1.Counter = 4
+            ///Prototype2_0.Counter = 5
+            ///Prototype2.1.Counter = 6
         }
 
         /// <summary>

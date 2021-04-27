66074bb803f6b46557f43937c1c41d73fbd8e685
YehonatanKadosh <jacojacoj3@gmail.com>
2021-04-27 22:06:47 +0300

    Right way to Composite Composite in Family Human Example

---
+++ Structural Pattern/Composite/Human.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace Composite
+{
+    partial class Program
+    {
+        class Human
+        {
+            public Human(string name, Human father, Human mother)
+            {
+                Father = father;
+                if (Father != null)
+                    Father.Children.Add(this);
+                Mother = mother;
+                if (Mother != null)
+                    Mother.Children.Add(this);
+                Name = name;
+            }
+            public string Name { get; set; }//                                   (Father)   (Mother)
+            public Human Father { get; }//                                           0         0
+            public Human Mother { get; }//                                             \     /
+            public List<Human> Children { get; set; } = new List<Human>();//          (Me)0
+            public override string ToString()//                                       //// \\\(Children)       
+            {
+                StringBuilder FamilyTree = new StringBuilder();//                    0000   000
+                if (Mother == null)
+                    if (Father == null)
+                        FamilyTree.Append("Parents is God");
+                    else
+                        FamilyTree.Append($"Father is {Father.Name} and mother's name unknown");
+                else if (Father == null)
+                    FamilyTree.Append($"Mother is {Mother.Name} and father's name is unknown");
+                else
+                    FamilyTree.Append($"Father is {Father.Name} and Mother is {Mother.Name}");
+                if (Children.Count > 0)
+                {
+                    FamilyTree.Append($"\n{Name}'s Childrens ");
+                    foreach (Human child in Children)
+                    {
+                        FamilyTree.Append($"{child.Name} ");
+                    }
+                }
+                return FamilyTree.ToString();
+            }
+
+        }
+    }
+}
---
+++ Structural Pattern/Composite/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            // Example of Composite is Humans
+            // Each individual can get to the other using his children or parents
+            Human Adam = new Human("Adam", null, null);
+            Human Eve = new Human("Eve", null, null);
+            Human Cain = new Human("Cain", Adam, Eve);
+            Human Abel = new Human("Abel", Adam, Eve);
+            Human Seth = new Human("Seth", Adam, Eve);
+            Human Enoch = new Human("Enoch", Cain, null);
+            Human Enos = new Human("Enos", Seth, null);
+            Console.WriteLine(Seth.ToString());
+
+            // Another example of composite is the FileSystem that allows you to move between Directories
+            // this way you can go to every file from every directory 
+            // going up "To the Parent directory" or down to the Childrens
+            
+            // Another example of composite is Xaml where it's built from UIElements
+            // Every Element contained within the Form "parent" and they can get talk to each other
+            // the Form have A Children property that allows them to interract one another
+        }
+    }
+}
